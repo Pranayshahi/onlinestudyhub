c2829baf5043f8d30c7d2720e509bc09e63f32dd
Pranayshahi <shahipran@gmail.com>
2026-04-19 09:51:53 +0530

    Configure Vercel serverless functions for Express backend

--- server/index.js
+++ server/index.js
@@ -270,4 +270,8 @@ function formatTeacher(t) {
   };
 }
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+if (require.main === module) {
+  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+}
+
+module.exports = app;
